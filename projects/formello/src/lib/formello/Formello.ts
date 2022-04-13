2eb93351c287600139926a514cf28c59e69141c3
Giuseppe Ettorre <g.ettorre@lendingsolution.it>
2022-04-13 18:08:47 +0200

    fix: agatha update

--- projects/formello/src/lib/formello/Formello.ts
+++ projects/formello/src/lib/formello/Formello.ts
@@ -1,58 +1,62 @@
-import { ThrowStmt } from "@angular/compiler";
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { Observable } from "rxjs";
-import { IFormelloConfig, IFormelloField } from "../../public-api";
-import { FormelloField } from "./FormelloField";
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { IFormelloConfig, IFormelloField } from '../../public-api';
+import { FormelloField } from './FormelloField';
 
-export class Formello<T>{
+export class Formello<T> {
+  private _config: IFormelloConfig<T>;
+  private _keys: Array<string> = [];
+  private _formGroup: FormGroup = new FormGroup({});
 
-    private _config: IFormelloConfig<T>;
-    private _keys: Array<string> = [];
-    private _formGroup: FormGroup = new FormGroup({});
+  constructor(config: IFormelloConfig<T>) {
+    this._config = config;
+    this.setKeysFromModel();
+    this.generateFormGroup();
+  }
 
-    constructor(config: IFormelloConfig<T>) {
-        this._config = config;
-        this.setKeysFromModel();
-        this.generateFormGroup();
-    }
+  private generateFormGroup() {
+    this._formGroup = new FormGroup({});
+    this.getFieldsFromRow().forEach((field) => {
+      if (field && field.name && field.control) {
+        this._formGroup.addControl(field.name, field.control);
+      }
+    });
+  }
 
-    private generateFormGroup() {
-        this._formGroup = new FormGroup({});
-        this.getFieldsFromRow().forEach(field => {
-            if (field && field.name && field.control) {
-                this._formGroup.addControl(field.name, field.control);
-            }
-        });
-    }
+  private getFieldsFromRow(): Array<IFormelloField> {
+    return this._config.rows.reduce(
+      (accumulator: any, currentValue) =>
+        accumulator.concat(currentValue.fields),
+      []
+    );
+  }
 
-    private getFieldsFromRow(): Array<IFormelloField> {
-        return this._config.rows.reduce((accumulator: any, currentValue) => accumulator.concat(currentValue.fields), []);
-    }
+  private setKeysFromModel() {
+    this._keys = Object.keys(this._config.model);
+  }
 
-    private setKeysFromModel() {
-        this._keys = Object.keys(this._config.model);
-    }
+  public getForm() {
+    return this._formGroup;
+  }
 
-    public getForm() {
-        return this._formGroup;
-    }
+  public getConfig() {
+    return this._config;
+  }
 
-    public getConfig() {
-        return this._config;
-    }
+  public changeConfiguration(config: IFormelloConfig<T>) {
+    //Persist model in rows set - to avoid data loss and valueChanges resubscriptions
+    this._config.rows = config.rows.map((row) => {
+      row.fields.forEach(
+        (field) =>
+          (field.control = (this._config.model as any)[field.name].control)
+      );
+      return row;
+    });
 
-    public changeConfiguration(config: IFormelloConfig<T>) {
-        //Persist model in rows set - to avoid data loss and valueChanges resubscriptions
-        this._config.rows = config.rows.map(row => {
-            row.fields.forEach(field => field.control = (this._config.model as any)[field.name].control);
-            return row;
-        });
+    this.generateFormGroup();
+  }
 
-        this.generateFormGroup();
-    }
-
-    public valueChanges(): Observable<any> {
-        return this.getForm().valueChanges;
-    }
-
+  public valueChanges(): Observable<any> {
+    return this.getForm().valueChanges;
+  }
 }
